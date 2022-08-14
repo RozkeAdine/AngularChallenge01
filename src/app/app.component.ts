96e20521fdba74e3d6fb58cf454a64f55df5ffca
RozkeAdine <105638291+RozkeAdine@users.noreply.github.com>
2022-08-14 16:52:05 +0200

    update file with title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  title = 'Bienvenue sur le site de Rozke';
 }
